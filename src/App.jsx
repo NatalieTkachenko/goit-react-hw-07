073dbda2ba647ff30371b6e06b11c5724cfc8f48
Natalie Tkachenko <tkachenko_nataliya83@ukr.net>
2024-03-20 23:15:21 +0200

    First commit. Project structure

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        All is good!
+        I will work for Google and make Google the client of NIX
+        I will live in luxury
+      </div>      
+    </>
+  )
+}
+
+export default App
